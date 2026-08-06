ff67bac2cf60b15f0a76d69926bc9ebd5bb83cc7
지유성 <20261844@edu.hanbat.ac.kr>
2026-08-06 15:28:02 +0900

    fix: resolve merge conflicts and clean up invalid resources

--- app/src/main/java/com/example/whs_after_mate/ui/auth/ResetPasswordFragment.java
+++ app/src/main/java/com/example/whs_after_mate/ui/auth/ResetPasswordFragment.java
@@ -4,11 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
 
 import com.example.whs_after_mate.databinding.FragmentResetPasswordBinding;
 
@@ -18,14 +16,9 @@ public class ResetPasswordFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        ResetPasswordViewModel resetPasswordViewModel =
-                new ViewModelProvider(this).get(ResetPasswordViewModel.class);
-
         binding = FragmentResetPasswordBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textResetPassword;
-        resetPasswordViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
--- app/src/main/java/com/example/whs_after_mate/ui/auth/SignUpFragment.java
+++ app/src/main/java/com/example/whs_after_mate/ui/auth/SignUpFragment.java
@@ -4,11 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
 
 import com.example.whs_after_mate.databinding.FragmentSignUpBinding;
 
@@ -18,14 +16,9 @@ public class SignUpFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        SignUpViewModel signUpViewModel =
-                new ViewModelProvider(this).get(SignUpViewModel.class);
-
         binding = FragmentSignUpBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textSignUp;
-        signUpViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
